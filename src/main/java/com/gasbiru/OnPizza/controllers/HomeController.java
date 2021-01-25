fe20adce5f32abe390480a4e80bcc5659adccb0a
Gabriel Garcez <procurando.gabriel@hotmail.com>
2021-01-25 17:14:45 -0300

    Login System

---
+++ src/main/java/com/gasbiru/OnPizza/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.gasbiru.OnPizza.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String login(){
+        return "login";
+    }
+}
