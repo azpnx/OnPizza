974b84f44eae3abaaa6f15d90ab05f6a3b8de5a2
Gabriel Garcez <procurando.gabriel@hotmail.com>
2021-01-25 16:23:51 -0300

    First Commit

---
+++ src/main/java/com/gasbiru/OnPizza/OnPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.gasbiru.OnPizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gasbiru/OnPizza/OnPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasbiru.OnPizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
